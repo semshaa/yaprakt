312abd09a366a0d82308a4ec9d040de198bf877f
semshaa <a.semshova@yandex.ru>
2025-04-12 14:08:51 +0400

    Update daysteps.go

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,20 +1,51 @@
-package daysteps
+ package daysteps
 
 import (
-	"time"
+    "fmt"
+    "strconv"
+    "strings"
+    "time"
+    "github.com/Yandex-Practicum/go1fl-4-sprint-final/spentcalories"
 )
 
-const (
-	// Длина одного шага в метрах
-	stepLength = 0.65
-	// Количество метров в одном километре
-	mInKm = 1000
-)
+const StepLength = 0.7 // Константа длины шага в метрах
 
+// parsePackage парсит строку данных вида "678,0h50m"
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+    parts := strings.Split(data, ",")
+    if len(parts) != 2 {
+        return 0, 0, fmt.Errorf("invalid data format")
+    }
+
+    steps, err := strconv.Atoi(parts[0])
+    if err != nil {
+        return 0, 0, fmt.Errorf("steps parse error: %w", err)
+    }
+
+    if steps <= 0 {
+        return 0, 0, fmt.Errorf("steps must be positive")
+    }
+
+    duration, err := time.ParseDuration(parts[1])
+    if err != nil {
+        return 0, 0, fmt.Errorf("duration parse error: %w", err)
+    }
+
+    return steps, duration, nil
 }
 
+// DayActionInfo формирует строку с информацией о дневной активности
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+    steps, duration, err := parsePackage(data)
+    if err != nil {
+        return fmt.Sprintf("Error: %v", err)
+    }
+
+    distanceKm := float64(steps) * StepLength / 1000
+    calories := spentcalories.KalkingSpentCalories(steps, weight, height, duration)
+
+    return fmt.Sprintf(
+        "Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.",
+        steps, distanceKm, calories,
+    )
 }
